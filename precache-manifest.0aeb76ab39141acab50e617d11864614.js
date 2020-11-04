89c39a76f5d22bc24a615289a124748b6c49e4d1
Aniket Kumar Verma <vermaaniket213@gmail.com>
2020-11-04 13:06:25 +0530

    Updates

---
+++ precache-manifest.0aeb76ab39141acab50e617d11864614.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8da9ee0cb888e8b35067bddd13eff9d",
+    "url": "/contact-list-live/index.html"
+  },
+  {
+    "revision": "d1c30c211e31f98550a8",
+    "url": "/contact-list-live/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "0a0583c1a3fc15482bbc",
+    "url": "/contact-list-live/static/css/main.53d4fc40.chunk.css"
+  },
+  {
+    "revision": "d1c30c211e31f98550a8",
+    "url": "/contact-list-live/static/js/2.ba8bb107.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/contact-list-live/static/js/2.ba8bb107.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a0583c1a3fc15482bbc",
+    "url": "/contact-list-live/static/js/main.05823a71.chunk.js"
+  },
+  {
+    "revision": "7ab76defb2faa10e1b70",
+    "url": "/contact-list-live/static/js/runtime-main.a37e712f.js"
+  }
+]);
